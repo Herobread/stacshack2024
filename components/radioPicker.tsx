55ef751f6f4bea3abdc6e07cac8755e27202088b
Alfio Tomarchio <at352@st-andrews.ac.uk>
2024-11-09 11:28:26 +0000

    style and add icon s

--- components/radioPicker.tsx
+++ components/radioPicker.tsx
@@ -1,6 +1,8 @@
-export default function RadioPicker(props: { set: (name: string) => void, radioGroup: string, name: string}) {
-    return <label>
+export default function RadioPicker(props: { set: (name: string) => void, radioGroup: string, name: string, icon:string, selected: string}) {
+    return <label className={props.selected == props.name ? "bg-gray-300" : "bg-transparent"}>
+        <img src={props.icon} className="h-32 m-auto"/>
+        <br/>
         <input type="radio" id={props.name} name={props.radioGroup} onClick={() => props.set(props.name)}/>
-        {props.name}
+        <p>{props.name}</p>
     </label>
 }
