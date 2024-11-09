bcb72904d580263009fdff23f78556d9939cdb0a
Alfio Tomarchio <at352@st-andrews.ac.uk>
2024-11-09 11:52:53 +0000

    declaration

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,18 @@ import { useState } from "react";
 export default function Home() {
 
   const [votePick, setvotePick] = useState("");
+  const candidates = [
+    {
+      name: "Donald Duck",
+      icon: "donald.png",
+      color: "red-400"
+    },
+    {
+      name: "Camilla Cabello",
+      icon: "camilla.png",
+      color: "blue-400"
+    }
+  ]
 
   return (
     <div className="max-w-screen-lg m-auto text-center">
@@ -13,9 +25,10 @@ export default function Home() {
 
       <h1>Choose your candidate:</h1>
 
-      <div className="grid grid-cols-2 gap-24">
-        <RadioPicker set={setvotePick} radioGroup="votePick" name="Donald Duck" icon="donald.png" selected={votePick}/>
-        <RadioPicker set={setvotePick} radioGroup="votePick" name="Camilla Cabello" icon="camilla.png" selected={votePick}/>
+      <div className={`grid grid-cols-${candidates.length} gap-24`}>
+        {candidates.map((cd) => 
+          <RadioPicker set={setvotePick} radioGroup="votePick" selected={votePick} candidate={cd}/>
+        )}
       </div>
 
       <p>You are for: {votePick} 🔫</p>
--- components/radioPicker.tsx
+++ components/radioPicker.tsx
@@ -1,8 +1,12 @@
-export default function RadioPicker(props: { set: (name: string) => void, radioGroup: string, name: string, icon:string, selected: string}) {
-    return <label className={props.selected == props.name ? "bg-gray-300" : "bg-transparent"}>
-        <img src={props.icon} className="h-32 m-auto"/>
+export default function RadioPicker(props: { set: (name: string) => void, radioGroup: string, selected: string, candidate: {
+    name: string,
+    icon: string,
+    color: string
+}}) {
+    return <label className={props.selected == props.candidate.name ? "bg-gray-300" : "bg-transparent"}>
+        <img src={props.candidate.icon} className="h-32 m-auto"/>
         <br/>
-        <input type="radio" id={props.name} name={props.radioGroup} onClick={() => props.set(props.name)}/>
-        <p>{props.name}</p>
+        <input type="radio" id={props.candidate.name} name={props.radioGroup} onClick={() => props.set(props.candidate.name)}/>
+        <p>{props.candidate.name}</p>
     </label>
 }
